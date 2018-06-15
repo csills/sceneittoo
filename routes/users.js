dffe1d83721edbc442f345e719e89345fe44c379
Kathryn Parker <kparker@warpedworks.com>
2018-06-14 21:54:47 -0400

    userAuth is working

--- routes/users.js
+++ routes/users.js
@@ -6,19 +6,19 @@ const User = require('../models/user');
 router.all('*', ensureAuthenticated);
 
 /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.render('users', {
-//     user: User.findById()
-//   });
-// });
-
 router.get('/', function(req, res, next) {
-  User.findById(req.user)
-  .then(user => {
-    res.render('users', {
-      user: user
-    });
-  })
+  res.render('users', {
+    user: User.findById()
   });
+});
+
+// router.get('/', function(req, res, next) {
+//   User.findById(req.user)
+//     .then(user => {
+//       res.render('users', {
+//         user: user
+//       });
+//     })
+//   });
 
 module.exports = router;
