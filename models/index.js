b6381a33f62d52912791f619179e5598008cacc5
Hyun Joo Nam <hyunjoooo.nam@gmail.com>
2018-07-19 19:45:07 -0400

    heroku trial3

--- models/index.js
+++ models/index.js
@@ -8,11 +8,8 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-
-if (process.env.DATABASE_URL) {
-  var sequelize = new Sequelize(process.env.DATABASE_URL);
-} else if (process.env.DB_HOST) {
-  var sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`);
+if (process.env.DB_HOST) {
+  var sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`);
 } else if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
